14072014b01a002aaaf591bbcdc309e616101896
Roman Kalyuzhnov <romankalyuzhnov@gmail.com>
2025-05-27 16:17:57 +0100

    lettering under landing page

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
     setTimeout(() => {
         document.body.classList.add("loaded");
-    }, 2500); // tempo em milissegundos (2.5s)
+    }, 3000); // tempo em milissegundos (2.5s)
 });
